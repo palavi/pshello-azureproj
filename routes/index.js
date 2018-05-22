33263e797b031b43803c252c30c2e8c62d119233
palavi <pshree2@sapient.com>
2018-05-22 09:25:19 +0530

    'reset'

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var router = express.Router();
+import { Router } from "express";
+var router = Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var model = { 
-    title: 'Express'
-    // message: process.env.MESSAGE || 'This is Development' 
+    title: 'Express',
+    message: process.env.MESSAGE || 'This is Development' 
   };
   res.render('index', model);
 });
 
-module.exports = router;
+export default router;
